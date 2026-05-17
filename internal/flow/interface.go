a1c85ba68df9873510084f61aaab7a4a6f84bc6f
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2026-05-16 20:50:30 -0500

    Add `GetFlowsByProduct` route and handler to fetch flows for a specific product

--- internal/flow/interface.go
+++ internal/flow/interface.go
@@ -5,4 +5,5 @@ import "net/http"
 type Handler interface {
 	CreateFlow(w http.ResponseWriter, r *http.Request)
 	GetFlowById(w http.ResponseWriter, r *http.Request)
+	GetFlowsByProduct(w http.ResponseWriter, r *http.Request)
 }
