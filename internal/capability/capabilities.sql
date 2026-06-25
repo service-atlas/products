d9ef3b5dbc08a44854f4041fb3f3450e21416b04
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2026-06-24 20:14:23 -0500

    Generate database layer for capabilities with CRUD operations using `sqlc`.

---
+++ internal/capability/capabilities.sql
@@ -0,0 +1,16 @@
+-- name: CreateCapability :one
+INSERT INTO capabilities(flow_id, name, description, created_at, updated_at)
+VALUES(@flow_id, @name, @description, @timestamp, @timestamp)
+RETURNING *;
+
+-- name: GetCapability :one
+SELECT id, flow_id, name, description, created_at, updated_at FROM capabilities WHERE id = @id;
+
+-- name: GetCapabilitiesByFlow :many
+SELECT id, flow_id, name, description, created_at, updated_at FROM capabilities WHERE flow_id = @flow_id;
+
+-- name: UpdateCapability :execrows
+UPDATE capabilities SET name = @name, description = @description, updated_at = @updated_at WHERE id = @id;
+
+-- name: DeleteCapability :execrows
+DELETE FROM capabilities WHERE id = @id;
