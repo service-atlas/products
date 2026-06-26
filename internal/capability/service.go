a2c95024a9a5637a438fb5a7cfc9d9a93e34c7aa
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2026-06-25 20:38:11 -0500

    Validate `FlowId` existence in `CreateCapability`, add error handling, and log creation failures

--- internal/capability/service.go
+++ internal/capability/service.go
@@ -2,6 +2,9 @@ package capability
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"log/slog"
 	"products/internal/capability/db"
 )
 
@@ -18,9 +21,24 @@ type postgresService struct {
 }
 
 func (s *postgresService) CreateCapability(ctx context.Context, req createCapabilityRequest) (db.Capability, error) {
+	name, err := s.queries.GetFlow(ctx, req.FlowId)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return db.Capability{}, NotFoundError{
+				"Flow not found",
+			}
+		}
+		return db.Capability{}, err
+	}
+	if name == "" {
+		return db.Capability{}, NotFoundError{
+			"Flow not found",
+		}
+	}
 	newCap, err := s.queries.CreateCapability(ctx, req.ToParams())
 	if err != nil {
-		return db.Capability{}, err
+		slog.Error("error creating capability: ", slog.String("error", err.Error()))
+		return db.Capability{}, errors.New("error creating capability")
 	}
 	return newCap, nil
 }
