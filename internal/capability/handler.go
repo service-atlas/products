819a80900b11208ef231e7dd52fb266c443d208e
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2026-06-28 15:18:37 -0500

    Add GetCapability and GetCapabilitiesByFlow API methods to service and handler

--- internal/capability/handler.go
+++ internal/capability/handler.go
@@ -3,6 +3,8 @@ package capability
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"log/slog"
 	"net/http"
 	"products/internal"
 	"products/internal/capability/db"
@@ -11,6 +13,8 @@ import (
 
 type Handler interface {
 	CreateCapability(w http.ResponseWriter, r *http.Request)
+	GetCapability(w http.ResponseWriter, r *http.Request)
+	GetCapabilitiesByFlow(w http.ResponseWriter, r *http.Request)
 }
 
 func NewHandler(dbConn db.DBTX) Handler {
@@ -45,4 +49,35 @@ func (h *capabilityHandler) CreateCapability(w http.ResponseWriter, r *http.Requ
 		return
 	}
 	internal.WriteJSONResponse(w, r, http.StatusCreated, newCap)
+}
+
+func (h *capabilityHandler) GetCapability(w http.ResponseWriter, r *http.Request) {
+	id, ok := internal.GetIntFromRequestPath("id", r)
+	if !ok {
+		internal.HandleHttpError(w, internal.ErrorEnvelope{Detail: "Invalid capability ID"}, http.StatusBadRequest)
+	}
+	capability, err := h.service.GetCapability(r.Context(), id)
+	if err != nil {
+		if errors.Is(err, internal.NotFoundError{}) {
+			internal.HandleHttpError(w, internal.ErrorEnvelope{Detail: "Capability not found"}, http.StatusNotFound)
+		}
+		slog.Error("Failed to fetch capability", slog.Int("capability_id", id), slog.String("error", err.Error()))
+	}
+	internal.WriteJSONResponse(w, r, http.StatusOK, capability)
+}
+
+func (h *capabilityHandler) GetCapabilitiesByFlow(w http.ResponseWriter, r *http.Request) {
+	id, ok := internal.GetIntFromRequestPath("id", r)
+	if !ok {
+		internal.HandleHttpError(w, internal.ErrorEnvelope{Detail: "Invalid capability ID"}, http.StatusBadRequest)
+	}
+	capabilities, err := h.service.GetCapabilitiesByFlow(r.Context(), id)
+	if err != nil {
+		if errors.Is(err, internal.NotFoundError{}) {
+			internal.HandleHttpError(w, internal.ErrorEnvelope{Detail: "Flow not found"}, http.StatusNotFound)
+		}
+		slog.Error("Failed to fetch capabilities", slog.Int("flow_id", id), slog.String("error", err.Error()))
+		internal.HandleHttpError(w, internal.ErrorEnvelope{Detail: "Failed to fetch capabilities"}, http.StatusInternalServerError)
+	}
+	internal.WriteJSONResponse(w, r, http.StatusOK, capabilities)
 }
