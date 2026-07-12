78419bd2a7d807a3489b0def55d8ea10b0968f27
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2026-07-11 21:13:42 -0500

    chore: update param name to req in CreateCapabilityStep method

--- internal/capability/service_capability_steps.go
+++ internal/capability/service_capability_steps.go
@@ -6,12 +6,12 @@ import (
 	"products/internal/capability/db"
 )
 
-func (s *postgresService) CreateCapabilityStep(ctx context.Context, step createCapabilityStepRequest) (db.CapabilityStep, error) {
-	_, err := s.GetCapability(ctx, step.CapabilityId)
+func (s *postgresService) CreateCapabilityStep(ctx context.Context, req createCapabilityStepRequest) (db.CapabilityStep, error) {
+	_, err := s.GetCapability(ctx, req.CapabilityId)
 	if err != nil {
-		return db.CapabilityStep{}, internal.NewNotFoundError(step.CapabilityId, "capability_step")
+		return db.CapabilityStep{}, internal.NewNotFoundError(req.CapabilityId, "capability_step")
 	}
-	capStep, err := s.queries.CreateCapabilityStep(ctx, step.ToParams())
+	capStep, err := s.queries.CreateCapabilityStep(ctx, req.ToParams())
 	if err != nil {
 		return db.CapabilityStep{}, err
 	}
